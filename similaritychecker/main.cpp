f18c06ed50112a3cf8020fb64575cde9302f75a9
sk.lee <moonstar11430@gmail.com>
2025-07-02 15:49:00 +0900

    same size test

--- similaritychecker/main.cpp
+++ similaritychecker/main.cpp
@@ -1,8 +1,17 @@
 #include "gmock/gmock.h"
 #include "similaritychecker.cpp"
 
-TEST(TS, TC1) {
-    EXPECT_EQ(1, 1);
+TEST(TS, LengthCheck) {
+	string input1 = "ABCD";
+	string input2 = "DCBA";
+
+	int score = 0;
+
+	SimChecker checker;
+
+	score = checker.getLengthSocre(input1, input2);
+
+    EXPECT_EQ(60, score);
 }
 int main()
 {
--- similaritychecker/similaritychecker.cpp
+++ similaritychecker/similaritychecker.cpp
@@ -1,3 +1,34 @@
-class SimilarityChecker {
+#include <string>
+#include <vector>
 
+using std::string;
+using std::vector;
+
+class SimChecker {
+public:
+	int getLengthSocre(string input1, string input2) {
+		int len1 = input1.length();
+		int len2 = input2.length();
+		int gap=0;
+		int shortest=0;
+	
+
+		if (len1 > len2) {
+			gap = len1 - len2;
+			shortest = len2;
+		}
+		else if(len1 < len2){
+			gap = len2 - len1;
+			shortest = len1;
+		}
+		else// len1 == len2
+		{
+			return 60;
+		}
+
+		double score = 0;
+		//score = shortest * (shortest - gap);
+		return (score*60);
+		
+	}
 };
